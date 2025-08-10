ce9223db474f87ca4a0d14ffe87bbe369a541fe2
Gal Zeira <gal_zeira@protonmail.com>
2025-08-10 02:33:06 -0400

    refactored hex to int conversion

--- penc.c
+++ penc.c
@@ -14,47 +14,14 @@ static bool is_hex(char c) {
 	return ('0' <= c && c <= '9') || ('A' <= c && c <= 'F') || ('a' <= c && c <= 'f');
 }
 
-static int ctox(char c) {
-	switch (c) {
-	case '0':
-		return 0;
-	case '1':
-		return 1;
-	case '2':
-		return 2;
-	case '3':
-		return 3;
-	case '4':
-		return 4;
-	case '5':
-		return 5;
-	case '6':
-		return 6;
-	case '7':
-		return 7;
-	case '8':
-		return 8;
-	case '9':
-		return 9;
-	case 'A':
-	case 'a':
-		return 10;
-	case 'B':
-	case 'b':
-		return 11;
-	case 'C':
-	case 'c':
-		return 12;
-	case 'D':
-	case 'd':
-		return 13;
-	case 'E':
-	case 'e':
-		return 14;
-	case 'F':
-	case 'f':
-		return 15;
-	default:
+static int xtoi(char c) {
+	if ('0' <= c && c <= '9') {
+		return c - '0';
+	} else if ('A' <= c && c <= 'F') {
+		return (c - 'A') + 10;
+	} else if ('a' <= c && c <= 'f') {
+		return (c - 'a') + 10;
+	} else {
 		return -1;
 	}
 }
@@ -88,8 +55,8 @@ size_t decode_file(FILE *file, char *inbuf, size_t incap, size_t inlen) {
 			} else {
 				// percent-decode
 				unsigned char n = 0;
-				n += ctox(inbuf[++i]) << 4;
-				n += ctox(inbuf[++i]);
+				n += xtoi(inbuf[++i]) << 4;
+				n += xtoi(inbuf[++i]);
 				outbuf[outlen++] = n;
 			}
 		}
